acf90f859bfb95ce210d310ae767ea90fcb3847b
watme <kurumsal@koddostu.com>
2015-10-28 11:14:58 +0300

    Create ulucore.js

---
+++ ulucore.js
@@ -0,0 +1,5 @@
+var fileref=document.createElement("link");
+        fileref.setAttribute("rel", "stylesheet");
+        fileref.setAttribute("type", "text/css");
+        fileref.setAttribute("href", "https://fonts.googleapis.com/css?family=Roboto:400,500&subset=latin-ext,latin");
+        document.getElementsByTagName("head")[0].appendChild(fileref);
